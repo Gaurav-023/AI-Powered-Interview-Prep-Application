69f0af197866895ff635b1f759c9ee045d9b88c3
Gaurav-023 <gauravsinghania812@gmail.com>
2026-06-25 00:15:06 +0530

    Setting up LandingPage CSS

--- frontend/interview-prep-ai/src/pages/LandingPage.jsx
+++ frontend/interview-prep-ai/src/pages/LandingPage.jsx
@@ -1,9 +1,64 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { APP_FEATURES } from "../utils/data";
 
 const LandingPage = () => {
+
+  // const navigate = useNavigate();
+  const [openAuthModal, setOpenAuthModal] = useState(false);
+  const [ currentPage, setCurrentPage ] = useState("login");
+  const  handleCTA = () => {};
+
   return (
-    <div>LandingPage</div>
+   <div className = "w-full min-h-full bg-[#FFFCEF]">
+      <div className = "w-[500px] h-[500px] bg-amber-200/20 blur-[65px] absolute top-0 left-0 "/>
+          <div className = "container mx-auto px-4 pt-6 pb-[200px] relative z-10">
+            {/* Header */}
+            <header className='flex justify-between items-center mb-16'>
+                <div className='text-xl text-black font-bold'>
+                  Interview Prep AI
+                </div>
+
+                <button className='bg-linear-to-r from-[#FF9324] to-[#e99a4b] text-sm font-semibold text-white px-7 py-2.5 rounded-full hover:bg-black hover:text-white border border-white transition-colors cursor-pointer' onClick={()=> setOpenAuthModal(true)}>
+                  Login / Sign Up
+                  </button>
+            </header>
+
+            {/* Hero Content */}
+            <div className='flex flex-col md:flex-row items-center'>
+              <div className='w-full md:w-1/2 pr-4 mb-8 md:mb-0'>
+                <div className='flex items-center justify-left mb-2'>
+                  <div className='flex items-center gap-2 text-[13px] text-amber-600 font-semibold bg-amber-100 px-3 py-1 rounded-full border border-amber-300'>
+                    AI Powered
+                  </div>
+                </div>
+
+                <h1 className='text-5xl text-black font-medium mb-6 leading-tight'>
+                  Ace Interview with <br/>
+                  <span className='text-transparent bg-clip-text '>
+                    AI-Powered
+                  </span>{" "}
+                  Learning
+                </h1>
+              </div>
+
+              <div className=''>
+                <p className=''>
+                  Get role-specific questions, expand answer when you need them,
+                  dive deeper into concepts, and organize everything your way.
+                  From preperation to mastery - your ultimate interview toolkit is here.
+                </p>
+                <button className=''
+                onClick={handleCTA}>
+                  Get Started
+                </button>
+
+              </div>
+            </div>
+
+          </div>
+      </div>
+
   )
 }
 
-export default LandingPage
+export default LandingPage;
