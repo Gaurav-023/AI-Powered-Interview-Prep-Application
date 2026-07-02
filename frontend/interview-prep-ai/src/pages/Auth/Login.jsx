af65173408c717fa97b4e73dee14519ef0a481e6
Gaurav-023 <gauravsinghania812@gmail.com>
2026-07-02 17:56:21 +0530

    Add Login SignUP Page and it Components

--- frontend/interview-prep-ai/src/pages/Auth/Login.jsx
+++ frontend/interview-prep-ai/src/pages/Auth/Login.jsx
@@ -1,8 +1,58 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import Input from '../../components/Inputs/Input';
 
-export const Login = () => {
+export const Login = ({setCurrentPage}) => {
+  const[email, setEmail] = useState("");
+  const[password, setPassword] = useState("");
+  const[error, setError] = useState(null);
+
+  const navigate = useNavigate();
+
+  //Handle Login Form Submit
+  const handleLogin = async(e) => {
+    e.preventDefault();
+  };
+
+
   return (
-    <div>Login</div>
+    <div className='w-[90vw] md:w-[33vw] p-7 flex flex-col justify-center'>
+      <h3 className='text-lg font-semibold text-black'> Welcome Back </h3>
+      <p className='text-xs'> Please Enter your Details to log in </p>
+
+      <form onSubmit={handleLogin}>
+        <Input
+        value={email}
+        onChange={({target}) => setEmail(target.value)}
+        label="Email Address"
+        placeholder="your@email.com"
+        type="text"/>
+
+        <Input 
+        value={password}
+        onChange={({ target }) => setPassword(target.value)} 
+        label="Password"
+        placeholder="Min 8 Characters"
+        type = "password" 
+        />
+
+
+      {error && <p className='text-red-500 text-xs pb-2.5'> {error} </p>}
+      <button type="submit" className='btn-primary'>
+        LOGIN
+      </button>
+
+      <p className='text-[13px] text-slate=800 mt-3'>
+        Don't have an account? {" "}
+      </p>
+        <button 
+        className='font-medium text-primary underline cursor-pointer'
+        onClick={ () => {
+          setCurrentPage("signup");
+        }} >SignUp</button>
+      </form>
+    </div>
+    
   )
 }
 
