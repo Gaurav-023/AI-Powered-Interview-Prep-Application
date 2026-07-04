b22b7dbe8a898d1b61942ced226d36ea23c58ab3
Gaurav-023 <gauravsinghania812@gmail.com>
2026-07-04 21:47:06 +0530

    Implement authentication features: add user registration, login, and profile retrieval; include middleware and route protection.

--- backend/server.js
+++ backend/server.js
@@ -2,8 +2,12 @@ require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const path = require("path");
+const connectDB = require("./config/db");
 
-const app = expres();
+const authRoutes = require("./routes/authRoutes");
+
+const app = express();
+
 
 //Middleware to handle CORS
 app.use(
@@ -14,7 +18,17 @@ app.use(
     })
 );
 
+connectDB();
 //Routes(will define it later)
+app.use("/api/auth", authRoutes);
+// app.use("/api/sessions", sessionRoutes);
+// app.use("/api/questions", questionRoutes);
+
+// app.use("/api/ai/generate-questions", protect, generateInterviewQuestions);
+// app.use("/api/ai/generate-explanation", protect, generateConceptExplaination);
+
+
+
 
 //Server uploads folder
 app.use("/uploads", express.static(path.join(__dirname, "uploads"), {}));
